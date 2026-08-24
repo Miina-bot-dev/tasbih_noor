5698c325852171335e8593bca9d132fcab283887
Miina-bot-dev <ramezanimina09@gmail.com>
2026-08-24 22:55:08 +0330

    Delete recipes/freetype/__init__.py

--- recipes/freetype/__init__.py
+++
@@ -1,9 +0,0 @@
-from pythonforandroid.recipes.freetype import FreetypeRecipe as _BaseFreetypeRecipe
-
-
-class FreetypeRecipe(_BaseFreetypeRecipe):
-    version = '2.13.2'
-    url = 'https://sourceforge.net/projects/freetype/files/freetype2/{version}/freetype-{version}.tar.gz/download'
-
-
-recipe = FreetypeRecipe()

90e9aeae6836bf4f34167f44c85d1e05b1eb4a06
Miina-bot-dev <ramezanimina09@gmail.com>
2026-09-02 16:10:11 +0330

    fix: final package fix

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
 from kivy.app import App
+from kivy.core.text import LabelBase
 from kivy.uix.label import Label
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FONT_FILE = os.path.join(BASE_DIR, "Vazirmatn-Regular.ttf")
+
+if os.path.exists(FONT_FILE):
+    LabelBase.register(name="Vazir", fn_regular=FONT_FILE)
 
 class TestApp(App):
     def build(self):
-        return Label(text="Hello Test")
+        return Label(text="Hello Test", font_name="Vazir")
 
 if __name__ == "__main__":
     TestApp().run()
